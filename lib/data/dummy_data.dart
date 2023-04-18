4c1e092ad93cd95db467870cc5792a7556d919bb
Barkley AIres <texwillian62@gmail.com>
2023-04-18 17:40:10 -0300

    Adicionando email na carga

--- lib/data/dummy_data.dart
+++ lib/data/dummy_data.dart
@@ -5,27 +5,27 @@ final dummyUsuarios = [
   Usuario(
     id: '1',
     nome: 'Maria',
-    email: 'A red shirt - it is pretty red!',
+    email: 'maria@gmail.com',
     imageUrl:
         'https://cdn.pixabay.com/photo/2016/09/01/08/24/smiley-1635449_960_720.png',
   ),
   Usuario(
     id: '2',
     nome: 'João',
-    email: 'A nice pair of trousers.',
+    email: 'joao@gmail.com',
     imageUrl:
         'https://cdn.pixabay.com/photo/2016/12/13/16/17/dancer-1904467_960_720.png',
   ),
   Usuario(
     id: '3',
     nome: 'Marcos',
-    email: 'Warm and cozy - exactly what you need for the winter.',
+    email: 'marcos@gmail.com',
     imageUrl: 'https://cdn.pixabay.com/photo/2016/09/01/08/25/smiley-1635464_960_720.png',
   ),
   Usuario(
     id: '',
     nome: 'Marta',
-    email: 'Prepare any meal you want.',
+    email: 'marta@gmail.com',
     imageUrl:
         'https://cdn.pixabay.com/photo/2013/07/12/19/15/gangster-154425_960_720.png',
   ),
